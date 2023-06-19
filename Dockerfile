f07136a5b9905ac45e8f8a1956dcb5443e6967bd
cglavin50 <cooperglavin@gmail.com>
2023-06-19 16:07:14 -0400

    backup

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.10-slim
+
+WORKDIR /slack-bot
+COPY requirements.txt requirements.txt
+RUN pip3 install -r requirements.txt
+# saves us the need for a virtualenv
+
+COPY . . 
+# copy everything else over
+
+# CMD ["python3", "-m", "flask", "run", "--host=0.0.0.0"]
+# test if can run without module mode
--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 slack_event_adapter = SlackEventAdapter(os.environ["SLACK_SIGNING_SECRET"], '/slack/events', app)
 
 # slack 
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+client = slack.WebClient(token=os.environ['SLACK_API_TOKEN'])
 bot_id = client.api_call("auth.test")['user_id'] # fetch bot information
 
 
@@ -22,20 +22,34 @@ bot_id = client.api_call("auth.test")['user_id'] # fetch bot information
 def message(payload):
     # print(payload)
     event = payload.get('event', {})
-    channel_id = event.get('channel')
-    uid = event.get('user')
-    # text = event.get('text')
-    files = event.get('files')
-    if files:
-        print("Files attached") # files is an array of attachments (files), each one a map
-        for item in files:
-            print(item) # should be 4th index
-    else:
-        print("No files")
-    # if uid != bot_id:
-        # client.chat_postMessage(channel=channel_id, text=text)
+    text = event.get('text')
 
-    
+    if text == "/leaderboards":
+        leaderboard_command(event.get('channel'))
+    else:
+        uid = event.get('user')
+        files = event.get('files')
+        if files:
+            print("Files attached") # files is an array of attachments (files), each one a map
+            update_counts(uid)
+
+        # if uid != bot_id:
+            # client.chat_postMessage(channel=channel_id, text=text)
+
+
+def leaderboard_command(channel_id):
+    # write a command to check the DB and send a leaderboard update to given channel
+    # should cache this as well
+    print("Implement this later")
+
+def update_counts(uid):
+    # this function should increment the count of a given user to the redis DB
+    response = client.users_profile_get(user=uid)
+    user_profile = response.get("profile")
+    real_name = user_profile.get("real_name")
+    display_name = user_profile.get("display_name")
+    print("Incrementing count for " + display_name + ", aka " + real_name)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
