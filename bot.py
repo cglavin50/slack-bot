ea725461c125fbde0a47b8baf0f07da0d44cf353
cglavin50 <cooperglavin@gmail.com>
2023-06-19 18:23:25 -0400

    Added update function

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 import slack
 import os
+import redis
 from pathlib import Path
 from dotenv import load_dotenv
 from flask import Flask
 from slackeventsapi import SlackEventAdapter
 
-# initialization
+# env initialization
 env_path = Path('.') / '.env' # stored in root directory
 load_dotenv(dotenv_path=env_path)
 
@@ -13,10 +14,23 @@ load_dotenv(dotenv_path=env_path)
 app = Flask(__name__)
 slack_event_adapter = SlackEventAdapter(os.environ["SLACK_SIGNING_SECRET"], '/slack/events', app)
 
-# slack 
+# slack init
 client = slack.WebClient(token=os.environ['SLACK_API_TOKEN'])
 bot_id = client.api_call("auth.test")['user_id'] # fetch bot information
 
+# redis init
+redis_client = redis.Redis(
+    host='localhost', # migrate this once reach deployment stage
+    port=6379,
+    charset='utf-8',
+    decode_responses=True,
+)
+
+response = redis_client.ping()
+print(response)
+
+
+# slack event handlers
 
 @slack_event_adapter.on('message')
 def message(payload):
@@ -42,14 +56,30 @@ def leaderboard_command(channel_id):
     # should cache this as well
     print("Implement this later")
 
-def update_counts(uid):
+def update_counts(uid, channel_id):
     # this function should increment the count of a given user to the redis DB
     response = client.users_profile_get(user=uid)
     user_profile = response.get("profile")
     real_name = user_profile.get("real_name")
     display_name = user_profile.get("display_name")
-    print("Incrementing count for " + display_name + ", aka " + real_name)
-
+    # temp
+    workout_channel = "temp id"
+    throwing_channel = "tempid2"
+    if channel_id == workout_channel:
+        print("Incrementing workout count for " + real_name + " ("+display_name+")")
+        key = real_name + " workout"
+    elif channel_id == throwing_channel:
+        print("Incrementing throwing count for " + real_name + " ("+display_name+")")
+        key = real_name + " throwing"
+    
+    print(key)
+    value = redis_client.get(real_name)
+    if value:
+        updated = int(value) + 1
+        print(redis_client.set(key, updated))
+    else:
+        print(redis_client.set(key, 1))
+# end update counts function
 
 if __name__ == "__main__":
     app.run(debug=True)
