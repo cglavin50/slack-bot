aa075a32b9628aafe8b08e9455e266426d8fb2e1
cglavin50 <cooperglavin@gmail.com>
2023-12-06 12:57:48 -0500

    Moving to test mode

--- bot.py
+++ bot.py
@@ -67,7 +67,7 @@ def parse_text(sender, txt): # takes in UID of the sender, and the text to see i
     user_profile = response.get("profile")
     real_name = user_profile.get("real_name")
     users = [real_name]
-    split = txt.strip('<>').split("@") # looking for any mentioned user IDs
+    split = txt.replace("<", " ").replace(">", " ").strip().split("@") # looking for any mentioned user IDs
     for str in split:
         try:
             response = client.users_profile_get(user = str)
@@ -199,7 +199,7 @@ def leaderboard_command(channel_id):
 def update_counts(names, channel_id, ts, text): # takes in array of user real names, and increments the keys accordingly
     real_name = names[0]
     for name in names:
-        workout_channel = os.environ["WORKOUT_ID"]
+        workout_channel = os.environ["C05J39BKG3S"] # TODO move to workout ID once working
         throwing_channel =  os.environ["THROW_ID"]
         key = ""
         if channel_id == workout_channel:
